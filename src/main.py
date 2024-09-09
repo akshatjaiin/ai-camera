d74f988af21b92974519a39539cedae31d01f8ac
manoj <manojpandawork@gmail.com>
2024-09-09 15:14:28 +0530

    add models

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 # import the opencv library 
 import cv2 
 import numpy as np
+from tflite import interpreter
 
 face_detector= cv2.CascadeClassifier('./models/haarcascade_frontalface_default.xml')
 gender_model = cv2.dnn.readNetFromCaffe('./models/gender.prototxt', './models/gender_net.caffemodel')
@@ -10,6 +11,7 @@ video =cv2.VideoCapture(0)
 
 def detectGender(frame):
     grayImg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    print("\nGray",grayImg ,"\n frame", frame)
     faces = face_detector.detectMultiScale(grayImg , scaleFactor=1.2, minNeighbors=5, minSize=(25, 25));
     for (x,y,w,h) in faces:
         face = frame[y:y+h, x:x+w]
@@ -23,7 +25,8 @@ def detectGender(frame):
         # Draw rectangles around the detected faces with the predicted gender label
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         cv2.putText(frame, f'{gender_label}', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-#
+
+
 while(True): 
     # Capture the video frame 
     # by frame 
@@ -35,7 +38,7 @@ while(True):
     # Display the resulting frame 
     cv2.imshow('frame', frame) 
       
-    if cv2.waitKey(1) & 0xFF == ord('q'):     break
+    if cv2.waitKey(1) & 0xFF == ord('q'): break
   
 # After the loop release the cap object 
 video.release() 
